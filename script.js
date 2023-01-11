664d809f1222524fac2fdd629939395d97fb75c7
Adam Chouhan <adam1370@stud.kea.dk>
2023-01-11 05:20:02 +0100

    5.5-6th commit

--- script.js
+++ script.js
@@ -6,9 +6,30 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
-// document.querySelectorAll(".nav-link").forEach((n) =>
-//   n.addEventListener("click", () => {
-//     hamburger.classList.remove("active");
-//     navMenu.classList.remove("active");
-//   })
-// );
+//Vis og fjern content fra tema 2
+let galleri = document.querySelector(".galleri");
+let change = true;
+
+document.querySelector(".btn").addEventListener("click", showGalleri);
+
+function showGalleri() {
+  galleri.classList.toggle("hide");
+}
+
+//Vis og fjern content fra tema 3
+let galleri2 = document.querySelector(".galleri2");
+
+document.querySelector(".btn2").addEventListener("click", showGalleri2);
+
+function showGalleri2() {
+  galleri2.classList.toggle("hide");
+}
+
+//Vis og fjern content fra tema 5
+let galleri3 = document.querySelector(".galleri3");
+
+document.querySelector(".btn3").addEventListener("click", showGalleri3);
+
+function showGalleri3() {
+  galleri3.classList.toggle("hide");
+}
